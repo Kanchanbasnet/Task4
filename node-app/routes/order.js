534f9f9e3f1555990b0a6d8a497689c5c6693219
Kanchan Basnet <131226662+Kanchanbasnet@users.noreply.github.com>
2023-10-01 22:01:43 +0545

    Update order.js

--- node-app/routes/order.js
+++ node-app/routes/order.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const {viewcart, getOrderById, addToCart, checkout, getOrder}= require('../controllers/OrderController.js');
 
 const orderRouter = express.Router();
-
-
-
-
-
-
-
+orderRouter.get('/', getOrder);
+//orderRouter.post('/addToCart',addToCart);
+//orderRouter.post('/checkout',checkout);
+orderRouter.get('/viewcart/:id',viewcart);
+orderRouter.get('/:id',getOrderById);
 
 
 
