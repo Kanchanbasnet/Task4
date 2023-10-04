ef290d8f9d24aba4a7b0fbed83642652dbf340f6
Kanchan Basnet <131226662+Kanchanbasnet@users.noreply.github.com>
2023-10-04 23:40:13 +0545

    Update OrderController.js

--- node-app/controllers/OrderController.js
+++ node-app/controllers/OrderController.js
@@ -1,64 +1,74 @@
-const order = require('../models/orderModel.js');
-const product = require('../models/productModel.js');
-const cart = require('../models/cartModel.js');
-const user = require('../models/userModel.js');
-
-
-exports.viewcart = async (req,res)=>{
-    try{
-       const cartId = req.params.id;
-       const cart = await cart.findOne({_id:cartId}).populate('products.product');
-       if(!cart){
-        return res.status(404).send("Cart does not exist.");
-       }
-       return res.status(200).json(cart);
+const User = require("../models/userModel");
+const Cart = require("../models/cartModel");
+const Product = require("../models/productModel");
+const Order = require("../models/orderModel");
 
 
 
+exports.checkout = async (req,res)=>{
+  try{
+    const cartId = req.params.id;
+    const cartExist = await Cart.findOne({cartId});
+    if(cartExist.price>500){
+      const newOrder = {
+        userId: cartExist.userId,
+        cartId,
+        items: cartExist.items,
+        price:cartExist.price
+      }
+      await Cart.findByIdAndRemove(cartId);
+      await Order.create(newOrder);
+      res.status(200).send("Order generated Successfully.");
 
     }
-    catch(error){
-        console.log(error);
-        res.status(500).send("Internal Server Error.")
+    else{
+      res.send({message:"Minimum order for checkout is 500."});
     }
+
+  }
+  catch(error){
+    console.log(error);
+    res.status(500).send({Error: 'Internal Server Error.'});
+  }
 }
 
-/*exports.viewCart = async (req, res) => {
-    const userId = req.params.id;
-    const cart = await Cart.findOne({ user: userId }).populate("products.product");
-    if (!cart) {
-        return res.status(404).send('Cart not found');
-    }
-    res.status(200).send(cart);
-};*/
 
 
 exports.getOrder = async (req,res) =>{
-    try{
-        const orders = await order.find().populate('user products.product');
-        if(orders.length === 0){
-            return res.status(200).send('You have no orders');
+  try{
+      const orders = await Order.find({}).populate('userId').populate('items.productId')
+      if(orders.length === 0){
+          return res.status(200).send('You have no orders');
 
-        }
-        return res.status(200).json(orders);
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).send('Internal Server Error.');
-    }
-    }
-    
+      }
+      return res.status(200).json(orders);
+  }
+  catch(error){
+      console.log(error);
+      res.status(500).send('Internal Server Error.');
+  }
+  }
+  
 exports.getOrderById = async (req,res) =>{
-    try{
-        const id = req.params.id;
-        const getOrder = await order.findOne({_id:id}).populate('user products.product');
-        if(!getOrder){
-            return res.status(404).json('Order id does not exist.');
-        }
-        res.status(200).send(getOrder);
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).send('Internal server Error');
-    }
+  try{
+      const orderId = req.params.id;
+      const getOrder = await Order.findById(orderId)
+      if(!getOrder){
+          return res.status(404).json('Order id does not exist.');
+      }
+      res.status(200).send(getOrder);
+  }
+  catch(error){
+      console.log(error);
+      res.status(500).send('Internal server Error');
+  }
 }
+
+
+
+
+
+
+
+
+
